ede2ff6b7e9621c3790706d00bffc675c970126a
Mary Kang <mary.kang09@gmail.com>
2020-02-09 23:25:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,5 +28,11 @@ return (age>=13 && age <=19) ? "You are a teenager" :
 }
 
 function switchAge(age) {
-
+switch(age) {
+  case 13 || 14 || 15 || 16 || 17 || 18 || 19 :
+  console.log("You are a teenager");
+  break;
+  default:
+  console.log("You have an age");
+}
 }
