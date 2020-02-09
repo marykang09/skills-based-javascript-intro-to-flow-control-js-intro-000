d312b814bd61ba17f31f856933107004a6a27790
Mary Kang <mary.kang09@gmail.com>
2020-02-09 22:51:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,6 +3,11 @@ function basicTeenager(age) {
 
 
 function teenager(age) {
+if (13 < age < 19) {
+  console.log "You are a teenager!"
+} else {
+  console.log "You are not a teenager"
+}
 }
 
 function ageChecker(age) {
